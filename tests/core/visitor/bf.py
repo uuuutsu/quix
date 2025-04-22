471132ca35de861b389dba064141ca7673ccc233
uuuutsu <124534001+uuuutsu@users.noreply.github.com>
2025-04-22 16:00:43 +0200

    feat: visitor tests

--- tests/core/visitor/bf.py
+++ tests/core/visitor/bf.py
@@ -1,41 +1,39 @@
-from typing import Final
+from io import StringIO
 
-import pytest
+from quix.core.opcodes.dtypes import Program
+from quix.core.opcodes.opcodes import add, inject, input, loop, output
+from quix.core.visitor.bf import BFVisitor
+from quix.core.visitor.bf.layout import BFMemoryLayout
+from quix.core.visitor.bf.pointer import BFPointer
 
-from quix.core.visitor.bf import BFMemoryLayout, BFPointer
-from quix.core.visitor.bf.commands import BFCommands
-from quix.exceptions.core import RefNotFound
-
-
-def test_pointer_default() -> None:
-    pointer = BFPointer.default()
-    assert pointer.position == 0
-
-    code = pointer.move_by_ptr(10)
-    assert code == BFCommands.MOVE_RIGHT * 10
-    assert pointer.position == 10
+L = BFMemoryLayout
 
 
-def test_pointer_ref() -> None:
-    START_POS: Final[int] = 3
-    REF_ONE_POS: Final[int] = 1
-    REF_TWO_POS: Final[int] = 5
-
-    layout = BFMemoryLayout({1: REF_ONE_POS, 2: REF_TWO_POS})
-    pointer = BFPointer(layout, start_position=START_POS)
-    assert pointer.position == START_POS
-
-    code = pointer.move_by_ref(2)
-    assert code == (BFCommands.MOVE_RIGHT if START_POS < REF_TWO_POS else BFCommands.MOVE_LEFT) * abs(
-        START_POS - REF_TWO_POS
-    )
-    assert pointer.position == REF_TWO_POS
+def test_add_pos() -> None:
+    assert compile([add(0, 5)], L({0: 1})) == ">+++++"
 
 
-def test_layout_no_ref() -> None:
-    REF_ONE_POS: Final[int] = 1
-    REF_TWO_POS: Final[int] = 5
+def test_add_neg() -> None:
+    assert compile([add(1, -3)], L({0: 4, 1: 2})) == ">>---"
 
-    layout = BFMemoryLayout({1: REF_ONE_POS, 2: REF_TWO_POS})
-    with pytest.raises(RefNotFound):
-        layout[3]
+
+def test_output() -> None:
+    assert compile([output(1)], L({0: 4, 1: 2})) == ">>."
+
+
+def test_input() -> None:
+    assert compile([input(1)], L({0: 4, 1: 2})) == ">>,"
+
+
+def test_loop() -> None:
+    assert compile([loop(3, [add(1, 1)])], L({3: 4, 1: 2})) == ">>>>[<<+>>]"
+
+
+def test_inject() -> None:
+    assert compile([inject(3, "(-_-)", 0), inject(3, "#", 0)], L({3: 4, 0: 2})) == ">>>>(-_-)>>#"
+
+
+def compile(program: Program, layout: BFMemoryLayout) -> str:
+    buff = StringIO()
+    BFVisitor(buff, BFPointer(layout)).visit(program)
+    return buff.getvalue()
---
+++ tests/core/visitor/layout.py
@@ -0,0 +1,15 @@
+from typing import Final
+
+import pytest
+
+from quix.core.visitor.bf import BFMemoryLayout
+from quix.exceptions.core import RefNotFound
+
+
+def test_layout_no_ref() -> None:
+    REF_ONE_POS: Final[int] = 1
+    REF_TWO_POS: Final[int] = 5
+
+    layout = BFMemoryLayout({1: REF_ONE_POS, 2: REF_TWO_POS})
+    with pytest.raises(RefNotFound):
+        layout[3]
