909c440d332814c6dd25ff25c0d67eb9688c3228
uuuutsu <124534001+uuuutsu@users.noreply.github.com>
2025-04-23 19:03:07 +0200

    feat: dispatch

--- quix/tools/__init__.py
+++ quix/tools/__init__.py
@@ -8,9 +8,13 @@ __all__ = (
     "SingletonMeta",
     #
     "generate_unique_id",
+    #
+    "DynamicOverload",
+    "dp_factory",
 )
 
 from .case import pascal_case_to_snake_case, snake_case_to_pascal_case
+from .dispatcher import DynamicOverload, dp_factory
 from .flyweight import FlyweightMeta, FlyweightStateType
 from .singleton import SingletonMeta
 from .unique import generate_unique_id
---
+++ quix/tools/dispatcher.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+from typing import Any, NoReturn
+
+from multimethod import multidispatch, multimethod
+
+
+def _dummy() -> NoReturn:
+    raise NotImplementedError("https://media.tenor.com/GNAQm8mZEPgAAAAM/oppenheimer-cillian-murphy.gif")
+
+
+class DynamicOverload[**P, R]:
+    __slots__ = ("_dispatcher",)
+
+    def __init__(
+        self,
+        func: Callable[P, R],
+        *,
+        dp: multimethod | None = None,
+    ) -> None:
+        if dp is not None:
+            self._dispatcher = dp
+            return
+
+        self._dispatcher = multidispatch(func)
+
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> R:
+        return self._dispatcher(*args, **kwargs)  # type: ignore
+
+    def __or__[**P_new, R_new](
+        self,
+        other: Callable[P_new, R_new] | DynamicOverload[P_new, R_new],
+    ) -> DynamicOverload[P, R] | DynamicOverload[P_new, R_new]:
+        return type(self)(other, dp=self._dispatcher)  # type: ignore
+
+
+def dp_factory[**P, R](
+    name: str,
+    *,
+    decs: list[Callable[[Callable[..., Any]], Callable[P, R]]],
+) -> DynamicOverload[[], NoReturn]:
+    dp = multidispatch(_dummy)
+
+    dp.__name__ = name
+    old_register = dp.register
+
+    def _new_register(func: Callable[P, R]) -> Callable[P, R]:
+        for dec in decs:
+            func = dec(func)
+        return old_register(func)
+
+    dp.register = _new_register  # type: ignore
+    return DynamicOverload(_dummy, dp=dp)
