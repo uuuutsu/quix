603344f7ed0958aeb6bbb222a87e2979b872ace0
uuuutsu <124534001+uuuutsu@users.noreply.github.com>
2025-04-25 20:38:16 +0200

    feat: scheduler

--- quix/scheduler/__init__.py
+++ quix/scheduler/__init__.py
@@ -17,6 +17,7 @@ __all__ = (
     "SliderRegistry",
     "create_slider_registry",
     "create_resolver_registry",
+    "Scheduler",
 )
 
 
@@ -25,4 +26,5 @@ from .constraints import Array, BaseConstraint, HardLink, Index, LifeCycle, Soft
 from .layout import Layout
 from .owner import Owner
 from .resolvers import MIPResolver, PrimitiveResolver, Resolver, ResolverRegistry, create_resolver_registry
+from .scheduler import Scheduler
 from .sliders import SimpleSlider, Slider, SliderRegistry, create_slider_registry
