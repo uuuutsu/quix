163d2baa638889974c9444e6878900b0f3a22a42
uuuutsu <124534001+uuuutsu@users.noreply.github.com>
2025-04-25 17:29:25 +0200

    feat: samplified layout

--- quix/scheduler/layout.py
+++ quix/scheduler/layout.py
@@ -7,23 +7,17 @@ class Layout:
     __slots__ = (
         "_mapping",
         "absolute",
-        "_reverse_mapping",
     )
 
     def __init__(self, absolute: bool = False) -> None:
         self._mapping: dict[Owner, int] = {}
-        self._reverse_mapping: dict[int, list[Owner]] = {}
         self.absolute = absolute
 
     def set(self, key: Owner, value: int) -> None:
         self._mapping[key] = value
-        self._reverse_mapping.setdefault(value, []).append(key)
 
-    def get_by_owner(self, key: Owner) -> int:
-        return self._mapping[key]
-
-    def get_by_idx(self, value: int) -> list[Owner]:
-        return self._reverse_mapping[value]
+    def mapping(self) -> dict[Owner, int]:
+        return self._mapping
 
     def __repr__(self) -> str:
         return pretty_repr(self._mapping)
