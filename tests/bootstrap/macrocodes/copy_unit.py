ce9b9820559177547cc1854c3c385debe2e3d2c4
uuuutsu <124534001+uuuutsu@users.noreply.github.com>
2025-04-27 19:34:05 +0200

    feat: copy tests

---
+++ tests/bootstrap/macrocodes/copy_unit.py
@@ -0,0 +1,20 @@
+from quix.bootstrap.dtypes import Int8, Unit
+from quix.bootstrap.macrocodes.copy_unit import copy_unit
+from quix.bootstrap.program import to_program
+from quix.core.opcodes.opcodes import add
+
+from .utils import run
+
+
+def test_move_unit_simple() -> None:
+    u1, u2 = Unit("u1"), Unit("u2")
+    program = to_program(
+        add(u1, 10),
+        add(u2, 30),
+        copy_unit(u1, {u2: Int8.from_value(-2)}),
+    )
+
+    mem = run(program)
+
+    assert mem[u1] == 10
+    assert mem[u2] == 10  # 30 + (-2 * 10)
