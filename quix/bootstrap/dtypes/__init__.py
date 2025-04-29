84e2c120a374ff51370b6badaef3d83e4a487fe0
uuuutsu <124534001+uuuutsu@users.noreply.github.com>
2025-04-29 04:03:05 +0200

    chore: removed Wide

--- quix/bootstrap/dtypes/__init__.py
+++ quix/bootstrap/dtypes/__init__.py
@@ -1,6 +1,5 @@
 __all__ = (
     "Unit",
-    "Wide",
     "DType",
     "Const",
     "UInt8",
@@ -10,4 +9,3 @@ __all__ = (
 from .base import DType
 from .const import Const, Int8, UInt8
 from .unit import Unit
-from .wide import Wide
