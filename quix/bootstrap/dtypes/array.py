e19d2a8026f502ab8c6b5dc235e7522f4a90a193
uuuutsu <124534001+uuuutsu@users.noreply.github.com>
2025-05-01 18:27:11 +0200

    fix: array full_length

--- quix/bootstrap/dtypes/array.py
+++ quix/bootstrap/dtypes/array.py
@@ -9,6 +9,6 @@ class Array(Unit):
 
     @property
     def full_length(self) -> int:
-        # N partitions + 1 control partition
+        # N partitions + 1 control partition + 2 safe zone partitions (for buffering)
         # each partition => [0][data1, data2, ...] => 1 + granularity
-        return (self.length + 1) * (self.granularity + 1)
+        return (self.length + 3) * (self.granularity + 1)
