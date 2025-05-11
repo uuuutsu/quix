39bb9ab4b0c86d31e1c83a54eeca6522ae4538cc
uuuutsu <124534001+uuuutsu@users.noreply.github.com>
2025-05-11 14:21:51 +0200

    feat: unit program

---
+++ quix/unit/program.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+
+from collections.abc import Callable, Generator, Iterable, Iterator
+from functools import wraps
+from typing import Self
+
+from rich.repr import Result, rich_repr
+
+from quix.core.opcodes import CoreOpcode, CoreProgram
+
+type ToConvert = (
+    CoreProgram | CoreOpcode | SmartProgram | Iterable[ToConvert] | Generator[ToConvert, None, ToConvert] | None
+)
+
+
+@rich_repr
+class SmartProgram:
+    __slots__ = ("_program",)
+
+    _program: list[CoreOpcode]
+
+    def __init__(
+        self,
+        program: list[CoreOpcode] | None = None,
+    ) -> None:
+        self._program = program or []
+
+    def __or__(self, other: ToConvert) -> Self:
+        if isinstance(other, SmartProgram):
+            self._program.extend(other._program)
+            return self
+
+        return self | to_program(other)
+
+    def __ror__(self, other: ToConvert) -> SmartProgram:
+        return to_program(other) | self
+
+    def build(self) -> CoreProgram:
+        return self._program
+
+    def __iter__(self) -> Iterator[CoreOpcode]:
+        return self._program.__iter__()
+
+    def __rich_repr__(self) -> Result:
+        yield from self._program
+
+
+def _unwrap_generator(func: Generator[ToConvert, None, ToConvert]) -> Iterable[ToConvert]:
+    yield (yield from func)
+
+
+def to_program(data: ToConvert, *include: ToConvert) -> SmartProgram:
+    if include:
+        data = (data, include)
+
+    match data:
+        case SmartProgram():
+            return data
+        case None:
+            return SmartProgram()
+        case CoreOpcode():
+            return SmartProgram([data])
+        case Generator():
+            return to_program(*_unwrap_generator(data))
+        case Iterable():
+            program: list[CoreOpcode] = []
+            for value in data:
+                new = to_program(value)
+                program.extend(new.build())
+            return SmartProgram(program)
+        case _:
+            raise ValueError(f"Trying to cast an unsupported data to OpCodeReturn. {type(data).__name__}: {data}")
+
+
+def convert[**P, R: ToConvert](func: Callable[P, R]) -> Callable[P, SmartProgram]:
+    @wraps(func)
+    def _wrapper(*args: P.args, **kwargs: P.kwargs) -> SmartProgram:
+        return to_program(func(*args, **kwargs))
+
+    return _wrapper
