5a1ff60365ebae409f6d9d7c1702f67bbf7c9891
uuuutsu <124534001+uuuutsu@users.noreply.github.com>
2025-05-23 19:27:53 +0200

    feat: constrs statable

--- quix/memoptix/scheduler/constraints/base.py
+++ quix/memoptix/scheduler/constraints/base.py
@@ -1,14 +1,21 @@
 from abc import ABC, abstractmethod
+from dataclasses import asdict, dataclass
+from typing import Any, Self
 
 from quix.memoptix.scheduler.owner import Owner
+from quix.tools.state import statable
 
 
+@dataclass(slots=True, frozen=True)
+@statable
 class BaseConstraint(ABC):
-    __slots__ = ()
-
     @abstractmethod
     def get_owners(self) -> set[Owner]:
         raise NotImplementedError
 
-    def __repr__(self) -> str:
-        return type(self).__name__
+    def __store__(self) -> dict[str, Any]:
+        return asdict(self)
+
+    @classmethod
+    def __load__(cls, data: dict[str, Any]) -> Self:
+        return cls(**data)
