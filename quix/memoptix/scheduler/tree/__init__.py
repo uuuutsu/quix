a5ac797529669016aad8c2eeb336b02a73ee7fc3
uuuutsu <124534001+uuuutsu@users.noreply.github.com>
2025-08-06 01:58:02 +0200

    chore: init

--- quix/memoptix/scheduler/tree/__init__.py
+++ quix/memoptix/scheduler/tree/__init__.py
@@ -12,11 +12,10 @@ __all__ = (
     "get_constraint_groups",
     "copy_node",
     "get_longest",
-    "split_root",
 )
 
 from .constraints import Array, BaseConstraint, HardLink, Index, SoftLink
 from .domain import Domain, get_constraint_groups, get_domain
 from .flatten import flatten_node
 from .node import Node, create_node
-from .utils import copy_node, get_longest, split_root
+from .utils import copy_node, get_longest
